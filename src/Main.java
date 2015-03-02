94a9edb1fa060b9dc4d3bd8f81450e9017963939
Jan Kohlček <kohlicekjan@gmail.com>
2015-03-02 16:27:15 +0100

    Přidání configu
    Veskere nastaveni bude v souboru Config.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("testovaci");
+            
+            System.out.println(Config.appName);
+            
 	}
-
 }
