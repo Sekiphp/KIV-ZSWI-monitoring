bbc61b066f14a55d87d0d78d2d4e07878c8c8955
Luboš Hubáček <seki@lostwar.cz>
2015-03-02 18:10:12 +0100

    Funkci nacteni URL
    nefunguje pro ostatni linky, nevim proc

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 
@@ -10,25 +11,18 @@ public class Main {
 	
 	private static void loadUrl() throws UnsupportedEncodingException, IOException{
 		URL url;
-		BufferedReader reader;
+		//BufferedReader br;
 		
-		for(int i = 0; i < Config.sourceArray.length; i++){
-			// testovani na prazdne pole
-			if(Config.sourceArray[i].equals(""))
-				continue;
-			
-			url = new URL(Config.sourceArray[i]);
-			System.out.println(Config.sourceArray[i]);
-			
-			
-			// samotne cteni
-			reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
-			    for (String line; (line = reader.readLine()) != null;) {
-			        System.out.println(line);
-			        System.out.println(" --- ");
-			    }
-			    reader.close();
-			
+		
+		url = new URL("http://peerfile.eu:3000/api/mon/memory_info");
+
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
+		    for (String line; (line = br.readLine()) != null;) {
+		        System.out.println(line);
+		    }
+		}
+		catch(Exception e){
+			System.out.println("spadlo ti to máchale, asi vítr");
 		}		
 	}
 
@@ -36,6 +30,18 @@ public class Main {
 		System.out.println(Config.appName);
 		
 		loadUrl();
+	
+		/*
+		URL url = new URL("http://peerfile.eu:3000/api/mon/memory_info");
+
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
+		    for (String line; (line = reader.readLine()) != null;) {
+		        System.out.println(line);
+		    }
+		}
+		catch(Exception e){
+			System.out.println("spadlo ti to máchale, asi vítr");
+		}*/
             
 	}
 }
