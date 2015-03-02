869afe8115e8327f6a88c802df8b6ec301c1cbd6
Luboš Hubáček <seki@lostwar.cz>
2015-03-02 15:49:45 +0100

    Init
    Zalozeny projekt

---
+++ src/app/Main.java
@@ -0,0 +1,20 @@
+package app;
+
+/**
+ * Main class of program
+ * 
+ * @author hubacek, pittl, kozler, homolka, kohlicek
+ *
+ */
+public class Main {
+
+	/**
+	 * 
+	 * @param args Arguments from command line
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
