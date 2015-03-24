dde372f7b9a12115752f5a7d406836f76212c901
Jan Kohlček <kohlicekjan@gmail.com>
2015-03-24 22:36:31 +0100

    Prototyp GUI

---
+++ src/library/InstanceId.java
@@ -0,0 +1,23 @@
+package library;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class InstanceId {
+	
+	/** logger sluzby InstanceId */
+	private static Logger instanceIdLogger = LogManager.getLogger();
+
+	private String instance_id;
+
+	public String getInstance_id() {
+		instanceIdLogger.info("Getting instance_id: " + instance_id);
+		return instance_id;
+	}
+
+	public void setInstance_id(String instance_id) {
+		instanceIdLogger.info("Setting instance_id: " + instance_id);
+		this.instance_id = instance_id;
+	}
+	
+}
--- src/ukazka/WebServiceClient.java
+++ src/ukazka/WebServiceClient.java
@@ -4,7 +4,7 @@ import java.io.StringReader;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
-import org.springframework.ws.client.core.WebServiceTemplate;
+//import org.springframework.ws.client.core.WebServiceTemplate;
 
 /**
  * Webservice client template. 
@@ -13,32 +13,32 @@ import org.springframework.ws.client.core.WebServiceTemplate;
 public class WebServiceClient {
 	
     private static final String MESSAGE = "<message xmlns=\"http://tempuri.org\">Hello World</message>";
-    private final WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
+    //private final WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
 
-    /**
-     * Sets custom default URL.
-     * @param defaultUri custom url
-     */
-    public void setDefaultUri(String defaultUri) {
-        webServiceTemplate.setDefaultUri(defaultUri);
-    }
-
-    /**
-     * Send to the configured default URI.
-     */
-    public void simpleSendAndReceive() {
-        StreamSource source = new StreamSource(new StringReader(MESSAGE));
-        StreamResult result = new StreamResult(System.out);
-        webServiceTemplate.sendSourceAndReceiveToResult(source, result);
-    }
-
-    /**
-     * Send to an explicit URI.
-     */
-    public void customSendAndReceive() {
-        StreamSource source = new StreamSource(new StringReader(MESSAGE));
-        StreamResult result = new StreamResult(System.out);
-        webServiceTemplate.sendSourceAndReceiveToResult("http://localhost:8080/AnotherWebService",
-            source, result);
-    }
+//    /**
+//     * Sets custom default URL.
+//     * @param defaultUri custom url
+//     */
+//    public void setDefaultUri(String defaultUri) {
+//        webServiceTemplate.setDefaultUri(defaultUri);
+//    }
+//
+//    /**
+//     * Send to the configured default URI.
+//     */
+//    public void simpleSendAndReceive() {
+//        StreamSource source = new StreamSource(new StringReader(MESSAGE));
+//        StreamResult result = new StreamResult(System.out);
+//        webServiceTemplate.sendSourceAndReceiveToResult(source, result);
+//    }
+//
+//    /**
+//     * Send to an explicit URI.
+//     */
+//    public void customSendAndReceive() {
+//        StreamSource source = new StreamSource(new StringReader(MESSAGE));
+//        StreamResult result = new StreamResult(System.out);
+//        webServiceTemplate.sendSourceAndReceiveToResult("http://localhost:8080/AnotherWebService",
+//            source, result);
+//    }
 }
