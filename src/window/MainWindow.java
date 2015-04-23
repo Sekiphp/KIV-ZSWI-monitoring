a42fdfb271bad4be7c3ae6946d908491ad01c172
Jan Kohlček <kohlicekjan@gmail.com>
2015-04-23 18:43:44 +0200

    Změna GUI

--- src/window/MainWindow.java
+++ src/window/MainWindow.java
@@ -13,16 +13,23 @@ import event.SaveAsFilter;
 import event.SaveFilter;
 import event.StartPauseMonitor;
 import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Separator;
 import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.ToolBar;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 import library.Monitoring;
 
@@ -52,16 +59,17 @@ public class MainWindow extends Application {
         this.monitoring = new Monitoring(console);
 
         BorderPane root = new BorderPane();
-        root.setTop(getMenuBar());
-        root.setLeft(monitoring.getFilter().getListView());
-        root.setCenter(getCenterContent());
+        root.setTop(this.getMenuBar());
+        root.setLeft(this.monitoring.getFilter().getListView());
+        root.setCenter(this.getCenterContent());
+        root.setBottom(this.getStatusBar());
 
         stage.setScene(new Scene(root, 800, 500));
-        
+
         File f = new File("src/application.css");
         stage.getScene().getStylesheets().clear();
         stage.getScene().getStylesheets().add("file:///" + f.getAbsolutePath().replace("\\", "/"));
-        
+
         stage.setTitle("Monitoring");
         stage.setMinHeight(400);
         stage.setMinWidth(400);
@@ -79,7 +87,7 @@ public class MainWindow extends Application {
     }
 
     private MenuBar getMenuBar() {
-    	Menu menu = new Menu("File");
+        Menu fileMenu = new Menu("File");
 
         MenuItem loadItem = new MenuItem("Load");
         loadItem.setMnemonicParsing(true);
@@ -94,27 +102,32 @@ public class MainWindow extends Application {
         MenuItem saveAsItem = new MenuItem("Save as...");
         saveAsItem.setMnemonicParsing(true);
         saveAsItem.setOnAction(new SaveAsFilter(this));
+    
+        MenuItem exitItem = new MenuItem("Exit");
+        exitItem.setMnemonicParsing(true);
+        exitItem.setOnAction(new ExitApp(this));
+        exitItem.getStyleClass().add("iconExit");
+
+        fileMenu.getItems().add(loadItem);
+        fileMenu.getItems().add(saveItem);
+        fileMenu.getItems().add(saveAsItem);
+        //fileMenu.getItems().add(startPauseItem);
+        fileMenu.getItems().add(new SeparatorMenuItem());
+        fileMenu.getItems().add(exitItem);
+        
+        
+        Menu monitorMenu = new Menu("Monitoring");
         
         MenuItem startPauseItem = new MenuItem("Start");
         startPauseItem.setMnemonicParsing(true);
         startPauseItem.setAccelerator(new KeyCodeCombination(KeyCode.SPACE));
         startPauseItem.setOnAction(new StartPauseMonitor(this.monitoring));
-
-        MenuItem exitItem = new MenuItem("Exit");
-        exitItem.setMnemonicParsing(true);
-        exitItem.setOnAction(new ExitApp(this));
-        exitItem.getStyleClass().add("iconExit");
-               
-
-        menu.getItems().add(loadItem);
-        menu.getItems().add(saveItem);
-        menu.getItems().add(saveAsItem);
-        menu.getItems().add(startPauseItem);
-        menu.getItems().add(new SeparatorMenuItem());
-        menu.getItems().add(exitItem);
-
+        
+        monitorMenu.getItems().add(startPauseItem);
+     
         MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().add(menu);
+        menuBar.getMenus().add(fileMenu);
+        menuBar.getMenus().add(monitorMenu);
         menuBar.prefWidthProperty().bind(stage.widthProperty());
         return menuBar;
     }
@@ -122,8 +135,35 @@ public class MainWindow extends Application {
     private TextArea getCenterContent() {
         this.console.setWrapText(true);
         this.console.setEditable(false);
-        this.console.setScrollTop(Double.MIN_VALUE);// scroluje na posledni radek
+        //this.console.setScrollTop(Double.MIN_VALUE);// scroluje na posledni radek
         return console;
+    }
+
+
+    private ToolBar getStatusBar() {
+
+        Region spring = new Region();
+        HBox.setHgrow(spring, Priority.ALWAYS);
+
+        CheckBox autoScroll = new CheckBox("Auto scroll");
+        autoScroll.setSelected(true);
+        autoScroll.selectedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+            if (newValue) {
+                //zapnuti autoscroll
+            } else {
+                //vypnuti autoscroll
+            }
+        });
+
+        ToolBar toolBar = new ToolBar();
+        toolBar.requestLayout();
+
+        toolBar.getItems().add(spring);// right
+        toolBar.getItems().add(autoScroll);
+        toolBar.getItems().add(new Separator());
+        toolBar.getItems().add(this.monitoring.getStatusLabel());
+
+        return toolBar;
     }
 
     /**
